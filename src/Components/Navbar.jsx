2dcb3c288b2312a272ed8959053bd93a0d6f36db
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-07-26 20:04:50 +0500

    little bug

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,52 +10,55 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   const { currentUser } = useSelector((state) => state.user);
   const { notifications } = useSelector((state) => state.user);
+  console.log(notifications);
 
   return (
     <div>
       <div className="navbar justify-between">
-
         <div>
-        <Link to="/">
-        <SiHomeadvisor className="h-12 w-12 text-[#4DBC15]" />
-        </Link>
-        </div>
-        
-        <div className="flex gap-10 sm:gap-16 items-center">
-
-        <Link to="/contract" >
-            <div>
-          <FaFileContract className="text-2xl mx-auto" />
-          <p className="text-sm">Contracts</p>
-            </div>
-        </Link>
-
-        <Link to="/notifications" className="">
-            <div className="flex relative ">
-          <FaBell className="text-2xl mx-auto" />
-          <p className="text-sm pr-2">Alerts</p>
-          {notifications.length!==0&& <p className="absolute w-5 h-5  text-center bg-green text-white rounded-full text-[10px] -top-3 -right-2 pt-[1px] ">
-            {notifications.length}1</p>}
-            </div>
-            
-        </Link>
-
-        <Link to="/chats" className="">
-            <div>
-          <IoChatboxEllipses className="text-[24px] mx-auto" />
-          <p className="text-sm">Messages</p>
-            </div>
-        </Link>
-
-        {currentUser ? (
-            <Dropdown img={currentUser.user.profilePicture}/>
-            ) : (
-                <Link to="/login" className="btn-dark py-2">
-            Login
+          <Link to="/">
+            <SiHomeadvisor className="h-12 w-12 text-[#4DBC15]" />
           </Link>
-        )}
         </div>
 
+        <div className="flex gap-10 sm:gap-16 items-center">
+          <Link to="/contract">
+            <div>
+              <FaFileContract className="text-2xl mx-auto" />
+              <p className="text-sm">Contracts</p>
+            </div>
+          </Link>
+
+          <Link to="/notifications" className="">
+            <div className="flex relative ">
+              <FaBell className="text-2xl mx-auto" />
+              <p className="text-sm pr-2">Alerts</p>
+              <p
+                className={
+                  "absolute w-5 h-5  text-center bg-green text-white rounded-full text-[10px] -top-3 -right-2 pt-[1px] " +
+                    (notifications?.length !== 0) && "hide"
+                }
+              >
+                {notifications?.length}
+              </p>
+            </div>
+          </Link>
+
+          <Link to="/chats" className="">
+            <div>
+              <IoChatboxEllipses className="text-[24px] mx-auto" />
+              <p className="text-sm">Messages</p>
+            </div>
+          </Link>
+
+          {currentUser ? (
+            <Dropdown img={currentUser.user.profilePicture} />
+          ) : (
+            <Link to="/login" className="btn-dark py-2">
+              Login
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
